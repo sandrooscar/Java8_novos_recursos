467e7ed8e5142ab3b19075dd0deaabcd464e68ce
Sandro Oscar Bugmann <prof.sandrob@gmail.com>
2020-05-09 11:16:18 -0300

    AULA03-Method reference

--- novosrecursos/src/main/java/br/com/java8/novosrecursos/OrdenaStrings.java
+++ novosrecursos/src/main/java/br/com/java8/novosrecursos/OrdenaStrings.java
@@ -3,6 +3,8 @@ package br.com.java8.novosrecursos;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class OrdenaStrings {
 	public static void main(String[] args) {
@@ -18,9 +20,24 @@ public class OrdenaStrings {
 				return 1;
 			return 0;
 		});
-		palavras.sort((String s1, String s2) -> Integer.compare(s1.length(), s2.length()));
+//		palavras.sort((String s1, String s2) -> Integer.compare(s1.length(), s2.length()));
+		palavras.sort(Comparator.comparing(s -> s.length())); 
+		palavras.sort(Comparator.comparing(String::length));  //method reference
+		
+		Function <String, Integer> funcao = String::length;
+		Function <String, Integer> funcao2 = s -> s.length();
+		
+		Comparator<String> comparador = Comparator.comparing(funcao);
+		
+		palavras.sort(comparador);
+		
 		System.out.println(palavras);
 		palavras.forEach(s -> System.out.println(s));
+		
+		Consumer<String> impressor = System.out::println;
+		palavras.forEach(impressor);
+		
+		palavras.forEach(System.out::println);
 	}
 }
 
