04f2b60a1fbe93df1d7bd8e7fbc22cd80c47d268
Sandro Oscar Bugmann <prof.sandrob@gmail.com>
2020-05-09 10:10:42 -0300

    AULA02-Que venham as lambdas-sintaxe

--- novosrecursos/src/main/java/br/com/java8/novosrecursos/ThreadsLambda.java
+++ novosrecursos/src/main/java/br/com/java8/novosrecursos/ThreadsLambda.java
@@ -3,7 +3,7 @@ package br.com.java8.novosrecursos;
 public class ThreadsLambda {
 
 	public static void main(String[] args) {
-		new Thread(() ->System.out.println("Executando um runnable")).run();
+		new Thread(() -> System.out.println("Executando um runnable")).run();
 	}
 
 }
